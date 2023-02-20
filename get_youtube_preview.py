2e6ce6f1712d8fbf113451a856dc858a6aff33b9
Maier Ludentrop <agamemnonagross@gmail.com>
2023-02-20 16:55:25 +0300

    first commit

---
+++ get_youtube_preview.py
@@ -0,0 +1,46 @@
+"""
+This script allows to download a youtube video preview by its link
+"""
+import requests as r
+import re
+
+
+def main() -> None:
+    url = input('URL: ')
+    download(get_video_id(url))
+
+
+def get_video_id(url: str) -> str:
+    """
+    The function extract a youtube video identifier
+
+    Args:
+        url (str): This is a video link
+
+    Returns:
+        (str): A video identifier
+    """
+
+    return re.findall(r".+[shorts | be]/(.{11})", url)[0]
+
+
+def download(video_id: str) -> None:
+    """
+    The function download a youtube video preview by its identifier
+
+    Args:
+        video_id (str): This is a video identifier
+
+    Returns:
+        None
+    """
+
+    data = r.get(f'https://img.youtube.com/vi/{video_id}/maxresdefault.jpg').content
+
+    with open(f'image-{video_id}.jpg', 'wb') as file:
+        file.write(data)
+
+
+
+if __name__ == '__main__':
+    main()
